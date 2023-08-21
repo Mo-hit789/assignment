eb6da9f7cc13674acc8323f0b3f7b9c1fabe8096
Mohit vishwakarma <mohitvishwakarma7891@gmail.com>
2023-08-21 10:05:46 +0530

    assignment1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { useState } from 'react';
+import SideBar from './component/SideBar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import img2 from './image/img2.jpeg';
+import statueImg from './image/img3.png';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+import './App.css'
+library.add(fas, faTwitter, faFontAwesome)
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`app-container ${showSidebar ? 'sidebar-visible' : ''}`}>
+      <div className="background-image"> 
+      <img 
+       className='centered-image1' 
+       src={img2} 
+       style={{width:"100vw",height:"100vh", objectFit: 'cover'}}
+       />
+      <img 
+      className={`centered-image2 ${showSidebar ? 'sidebarOpen' : ''}`}
+      src={statueImg} 
+      />
+      </div>
+      <div  className={`toggle-button ${showSidebar ? 'sidebar-visible' : ''}`} >
+        <button onClick={toggleSidebar}>
+          {
+            showSidebar ?
+            <FontAwesomeIcon icon="fa-solid fa-angles-left" style={{color:"#984211"}} /> :
+            <FontAwesomeIcon icon="fa-solid fa-angles-right" style={{color:"#984211"}}  />
+          }
+           </button>
+      </div>
+      { showSidebar && <SideBar  toggleSidebar = {toggleSidebar} />}
     </div>
   );
+  
 }
 
 export default App;
---
+++ src/component/SideBar.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./SideBar_style.css";
+
+
+const  SideBar = ({toggleSidebar}) => {
+const [readMore,setReadMore] = useState(false);
+const [readmoreorless,setreadmoreorless] = useState("More");
+   const readmore = () =>{
+    setreadmoreorless(readmoreorless === "More" ?'Less':'More' );
+        setReadMore(readmoreorless === "More" ? true:false  );
+   }
+ 
+  return (
+
+    <div className={`sidebar ${readMore ? 'readmore' : ''}`}>
+    <button onClick={toggleSidebar} style={{position:'absolute', left:'20px', top:'20px', border:'0',cursor:'pointer'}}>
+        <FontAwesomeIcon icon="fa-solid fa-chevron-left" className='icon circle-icon' />
+        </button>
+      <div className='content'>
+           <div>
+        <h2 style={{letterSpacing: "3px"}}>
+          MAITREYA BUDHHA <br/>
+          IN GESTURE OF <br/>
+          FEARLESNESS <br/>
+          (ABHAY MUDRA)
+        </h2>
+       </div>
+
+       <div className='icon-container'>
+       <FontAwesomeIcon icon="fa-solid fa-heart" className='icon circle-icon'/>
+       <FontAwesomeIcon icon="fa-solid fa-share-nodes"  className='icon circle-icon'/>
+       <FontAwesomeIcon icon="fa-solid fa-eye"  className='icon circle-icon'/>
+       </div>
+       <div className='count'>
+        <span>0</span>
+        <span>120</span>
+        <span>27</span>
+        
+       </div>
+       <div style={{color:"#717171"}}>
+      Dynasty: ahichhchhatra <br/>
+      Period: 200 CE <br/>
+      Material: stone <br/>
+      Location: National Museum 
+    </div>
+
+     <div>
+      <h3>DESCRIPTION:</h3>
+      <p style={{color:"#717171",margin: "0 auto", }}>Maitreya, the future Buddha to be, resides in the Tushita heaven as a 
+        bodhisattva waiting to redeem humanity.    
+        
+        </p>
+        {
+          readMore && <p 
+          style={{
+            color: "#717171",
+            fontSize: "16px", // Set a default font size
+            maxWidth: "100%",
+            boxSizing: "border-box",
+            margin: "0 auto",
+            // padding: "10px",
+            justifyContent:"center",
+          }}
+          >In Buddhism, Maitreya is the eighth Buddha, 
+          a successor of the seven historical Buddhas sapta-manushi 
+          Buddhas. bodhisattva waiting to redeem humanity. In Buddhism, Maitreya is the eighth Buddha, 
+          a successor of the seven historical Buddhas sapta-manushi 
+          Buddhas.
+          bodhisattva waiting to redeem humanity. In Buddhism, Maitreya is the eighth Buddha, 
+          a successor of the seven historical Buddhas sapta-manushi 
+          Buddhas</p>
+        }
+        <FontAwesomeIcon icon="fa-solid fa-arrow-right" style={{color:"#984211", fontWeight:"bold"}}/> {` `}
+        <button onClick={()=> readmore()} style={{border:"0", color:"#984211", fontWeight:"bold",background: "none", cursor:"pointer"}}><a>Read {readmoreorless}</a></button> 
+     </div>  
+     <div className='btn' style={{marginTop:"10px"}}>
+     <button className='btn1'>ADD TO COLLECTION</button> {` `}
+     <button className='btn2'>SOUVENIR</button>
+     </div>
+     
+     </div>  
+    
+   </div>
+  );
+};
+
+export default SideBar;
